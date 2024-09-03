43b17f1a55af29e3aef4e5141eec7ddd409967e4
--add <--add>
2024-09-03 18:29:23 +0530

    fifth line

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const age = 26;
 const name = 'sagar';
 
 const dob = '01/01/2024'
-function myfunk() {
-    console.log("this is a function5");
-    
+function myfunk3() {
+  console.log("this is a function");
 }
